3fd14dc7f9bb4c52993492c752954555bab37a8c
Eduardo Cruz Guedes <eduardo9cruz@gmail.com>
2022-11-11 18:13:48 +0000

    Minor importation fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Login from "./pages/Login";
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { RequireAuth } from "./contexts/auth/RequireAuth";
 import Vehicles from "./pages/Vehicles";
--- src/pages/Vehicles/index.tsx
+++ src/pages/Vehicles/index.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import Header from "../../components/Header";
 import { Wrapper, Title } from "./styles";
 
